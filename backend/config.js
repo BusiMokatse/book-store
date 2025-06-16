306673f8802e1ac8cd1a33f35db219519177c1ba
Busisiwe Mokatse <busisiwe@koedr.net>
2025-06-16 21:09:11 +0200

    Oops

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 export const mongoDBURL = 
-    'mongodb+srv://busimokatse:H2hWvKcuuvaxCUi7@book-store-mern.ofmscbd.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
+   
     
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,21 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
+
+// Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS Policy
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+  })
+)
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome to MERN Stack Tutorial');
@@ -14,6 +25,7 @@ app.get('/', (request, response) => {
 })
 
 app.use('/books', booksRoute);
+
 mongoose
   .connect(mongoDBURL)
   .then(() => { 
