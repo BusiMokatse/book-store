744559c16de7a3bf718f2893e32257993c3ac570
Busisiwe Mokatse <busisiwe@koedr.net>
2025-06-29 15:38:24 +0200

    Add react router dom

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,14 @@ const app = express();
 app.use(express.json());
 
 // Middleware for handling CORS Policy using custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"]
-  })
-)
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"]
+//   })
+// )
+
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome to MERN Stack Tutorial');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import CreateBook from './pages/CreateBook';
-import EditBook from './pages/EditBook';
-import DeleteBook from './pages/DeleteBook';
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
 import ShowBook from './pages/ShowBook'
-
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='books/details/:id' element={<ShowBook />} />
-      <Route path='books/edit/:id' element={<EditBook />} />
-      <Route path='books/delete/:id' element={<DeleteBook />} />
+    <Route path="/" element={<Home />}/>
+    <Route path="/book/create" element={ <CreateBooks />}/>
+    <Route path="/book/delete/:id" element={<DeleteBook />}/>
+    <Route path="/book/edit/:id" element={<EditBook />}/>
+    <Route path="/book/details/:id" element={<ShowBook />}/>
+    
     </Routes>
   )
 }
